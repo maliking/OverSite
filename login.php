8f8b4a59061877ce70df61e8081a308859eba7cb
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-05-30 18:52:51 -0500

    Changed input type for username from email to text

--- login.php
+++ login.php
@@ -35,7 +35,7 @@
 
       <form action="index.php" method="post">
         <div class="form-group has-feedback">
-          <input type="email" class="form-control" placeholder="Username" name="userName">
+          <input type="text" class="form-control" placeholder="Username" name="userName">
           <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
