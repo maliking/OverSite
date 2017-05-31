d0ddab6f230732c22e4de08d2e6148313286e9cf
Jose Diaz <jodiaz@csumb.edu>
2017-05-30 22:26:51 -0700

    Testing index.php php code

--- index.php
+++ index.php
@@ -10,39 +10,39 @@
 
     $dbConn = getConnection();
 
-    $username = $_POST['userName'];
-    $password = sha1($_POST['password']);
+    // $username = $_POST['userName'];
+    // $password = sha1($_POST['password']);
     
-    $sql = "SELECT * FROM UsersInfo WHERE username = :username AND password = :password";
-    $namedParameters = array();
-    $namedParameters[':userame'] = $username;
-    $namedParameters[':password'] = $password;
-    $stmt = $dbConn -> prepare($sql);
-    $stmt->execute($namedParameters);
-    //$stmt->execute();
-    $result = $stmt->fetch(); //We are expecting one record
-    if (empty($result)) 
-    {
-         header("Location: index.html?error=WRONG USERNAME OR PASSWORD");
-    }
-    else 
-    {
-      session_start();
+    // $sql = "SELECT * FROM UsersInfo WHERE username = :username AND password = :password";
+    // $namedParameters = array();
+    // $namedParameters[':userame'] = $username;
+    // $namedParameters[':password'] = $password;
+    // $stmt = $dbConn -> prepare($sql);
+    // $stmt->execute($namedParameters);
+    // //$stmt->execute();
+    // $result = $stmt->fetch(); //We are expecting one record
+    // if (empty($result)) 
+    // {
+    //      header("Location: login.php?error=WRONG USERNAME OR PASSWORD");
+    // }
+    // else 
+    // {
+    //   session_start();
         
-      $_SESSION['userId']  = $result['userId'];
-      $_SESSION['username'] = $result['username'];
-      //$_SESSION['userName'] = $result['firstName'] . " " . $result['lastName'];
-      //$_SESSION['userId'] = $result['userId'];
-      if($result['userType'] == 1)
-      {
-        header("Location: Agent/IDXGetFeatured.php");
-      }
-      else if($result['userType'] == 0)
-      {
-        header("Location: Admin/AdminProfile.php");
-      }
+    //   $_SESSION['userId']  = $result['userId'];
+    //   $_SESSION['username'] = $result['username'];
+    //   //$_SESSION['userName'] = $result['firstName'] . " " . $result['lastName'];
+    //   //$_SESSION['userId'] = $result['userId'];
+    //   if($result['userType'] == 1)
+    //   {
+    //     header("Location: Agent/IDXGetFeatured.php");
+    //   }
+    //   else if($result['userType'] == 0)
+    //   {
+    //     header("Location: Admin/AdminProfile.php");
+    //   }
         
-    }
+    // }
   }
 
 ?>
