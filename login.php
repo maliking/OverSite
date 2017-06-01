0cb7a52977617508e3bed9260659166f1bfa9d5b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-05-31 20:01:04 -0500

    Fixed variable name for username from login.php

--- login.php
+++ login.php
@@ -35,7 +35,7 @@
 
       <form action="validateLogin.php" method="post">
         <div class="form-group has-feedback">
-          <input type="text" class="form-control" placeholder="Username" name="userName">
+          <input type="text" class="form-control" placeholder="Username" name="username">
           <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
