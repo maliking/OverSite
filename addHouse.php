33759749e0429f34b8257749980a54753561e1be
Jose Diaz <jodiaz@csumb.edu>
2017-06-20 22:41:38 -0700

    adding add house file

--- addHouse.php
+++ addHouse.php
@@ -3,177 +3,115 @@
     session_start();
     $dbConn = getConnection();
     if(!isset($_SESSION['userId'])) {
-        header("Location: userLogin.php?error=wrong username or password");
+      header("Location: userLogin.php?error=wrong username or password");
     } 
+    $houseId = $_GET['houseId'];
  ?>
-
-
+ 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Add New House Information</title>
-    <meta charset = "utf-8"/>
-    <link type="text/css" rel="stylesheet" href="addOrEditInfo.css">
-    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    <script>
-        /*function sendAddressCounty() {  
-          
-         $.ajax({
-                type: "get",
-                url: "../PQLogin/runScript.php",
-                dataType: "json",
-                data: {"county": $("#county").val(),
-                        "address": $("#address").val() },
-                success: function(data,status) {
-                    //alert(status);
-                    document.getElementById('bedrooms').value = data['bedrooms']; //added for testing purposes on how the items will be added to form later on
-                    document.getElementById('bathrooms').value = data['bathrooms'];
-                    document.getElementById('price').value = data['price'];
-                },
-                complete: function(data,status) { //optional, used for debugging purposes
-                     //alert(JSON.stringify(data));
-                }
-             });
-         }*/
-    </script><!-- importing jQuery library-->
-    <style type="text/css">
-        .form select {
-          font-family: "Roboto", sans-serif;
-          outline: 0;
-          background: #f2f2f2;
-          width: 100%;
-          border: 0;
-          margin: 0 0 15px;
-          padding: 15px;
-          box-sizing: border-box;
-          font-size: 14px;
-          text-align: center;
-        }
-    </style>
+  <meta charset="utf-8">
 
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Sign-Up Form</title>
+  <link href="styles.css" rel="stylesheet"/>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!--<meta name="viewport" content="width=device-width; initial-scale=1.0">-->
+
+  <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+  
+  <script>     
+      
+      function displayError(id, message){
+        $(id + "Error").html(message);
+        $(id).css("background-color","red");
+        $(id).focus();
+        $(id + "Error").css("color", "red");
+      }
+      
+      function checkPhone(){
+          if(!/^\d{3}\s*\d{3}\d{4}$/.test($("#phone").val())) 
+              {
+              displayError("#phone", "Please enter a 10-digit phone number");
+              return false;      
+              }
+          else
+              {
+                  $("#phoneError").html("");
+                  $("#phone").css("background-color","#66FF66");
+              }
+      }
+      
+  </script>
+  
 </head>
-    <body>
-                <!-- Navigation Bar -->
-            <?php
-                // require("agentNav.php");
-            ?>  
-            
-                <div type="text" class="form" style="float:left; width:50%;">
-                    <h1>Enter House Information</h1>
-                    Status:
-                    <select id="status">
-                      <option value="active" selected>active</option>
-                      <option value="pending">pending</option>
-                      <option value="sold">sold</option>
-                    </select>
-                    County:
-                    <select id="county">
-                      <option value="Alameda, CA">Alameda, CA</option>
-                      <option value="Alpine, CA">Alpine, CA</option>
-                      <option value="Amador, CA">Amador, CA</option>
-                      <option value="Butte, CA">Butte, CA</option>
-                      <option value="Calaveras, CA">Calaveras, CA</option>
-                      <option value="Colusa, CA">Colusa, CA</option>
-                      <option value="Contra Costa, CA">Contra Costa, CA</option>
-                      <option value="Del Norte, CA">Del Norte, CA</option>
-                      <option value="El Dorado, CA">El Dorado, CA</option>
-                      <option value="Fresno, CA">Fresno, CA</option>
-                      <option value="Glenn, CA">Glenn, CA</option>
-                      <option value="Humboldt, CA">Humboldt, CA</option>
-                      <option value="Imperial, CA">Imperial, CA</option>
-                      <option value="Inyo, CA">Inyo, CA</option>
-                      <option value="Kern, CA">Kern, CA</option>
-                      <option value="Kings, CA">Kings, CA</option>
-                      <option value="Lake, CA">Lake, CA</option>
-                      <option value="Lassen, CA">Lassen, CA</option>
-                      <option value="Los Angeles, CA">Los Angeles, CA</option>
-                      <option value="Madera, CA">Madera, CA</option>
-                      <option value="Marin, CA">Marin, CA</option>
-                      <option value="Mariposa, CA">Mariposa, CA</option>
-                      <option value="Mendocino, CA">Mendocino, CA</option>
-                      <option value="Merced, CA">Merced, CA</option>
-                      <option value="Modoc, CA">Modoc, CA</option>
-                      <option value="Monterey, CA" selected>Monterey, CA</option>
-                      <option value="Napa, CA">Napa, CA</option>
-                      <option value="Nevada, CA">Nevada, CA</option>
-                      <option value="Orange, CA">Orange, CA</option>
-                      <option value="Placer, CA">Placer, CA</option>
-                      <option value="Plumas, CA">Plumas, CA</option>
-                      <option value="Riverside, CA">Riverside, CA</option>
-                      <option value="Sacramento, CA">Sacramento, CA</option>
-                      <option value="San Benito, CA">San Benito, CA</option>
-                      <option value="San Bernardino, CA">San Bernardino, CA</option>
-                      <option value="San Diego, CA">San Diego, CA</option>
-                      <option value="San Francisco, CA">San Francisco, CA</option>
-                      <option value="San Joaquin, CA">San Joaquin, CA</option>
-                      <option value="San Luis Obispo, CA">San Luis Obispo, CA</option>
-                      <option value="San Mateo, CA">San Mateo, CA</option>
-                      <option value="Santa Barbara, CA">Santa Barbara, CA</option>
-                      <option value="San Clara, CA">San Clara, CA</option>
-                      <option value="Santa Cruz, CA">Santa Cruz, CA</option>
-                      <option value="Shasta, CA">Shasta, CA</option>
-                      <option value="Sierra, CA">Sierra, CA</option>
-                      <option value="Siskiyou, CA">Siskiyou, CA</option>
-                      <option value="Solano, CA">Solano, CA</option>
-                      <option value="Sonoma, CA">Sonoma, CA</option>
-                      <option value="Stanislaus, CA">Stanislaus, CA</option>
-                      <option value="Tehama, CA">Tehama, CA</option>
-                      <option value="Trinity, CA">Trinity, CA</option>
-                      <option value="Tulare, CA">Tulare, CA</option>
-                      <option value="Tuolumne, CA">Tuolumne, CA</option>
-                      <option value="Ventura, CA">Ventura, CA</option>
-                      <option value="Yolo, CA">Yolo, CA</option>
-                      <option value="Yuba, CA">Yuba, CA</option>
-                    </select>
-                    <input type="text" id="address" placeholder="address"> <br />
-                    <input type="text" id="city" placeholder="city"><br />
-                    <!-- City: <span id="cities"></span> <br /> -->
-                    <input type="text" id="state" value="California" readonly=""><br />
-                    <input type="text" id="zip" placeholder="zip"><br />
-                    <input type="text" id="bedrooms" placeholder="bedrooms"><br />
-                    <input type="text" id="bathrooms" placeholder="bathrooms"><br />
-                    <input type="text" id="price" placeholder="price"><br />
-                    <input type="hidden" id="userId" value="<?=$_SESSION['userId']?>"> 
-                    <input id="button" type="button" value="Enter" onclick="enterInfo()"> 
-                    <?php include('../footer.php'); ?>
-                    
-            
-            
-                    <script> 
-                        $("#address").change(sendAddressCounty());
-                        function enterInfo(){
-                          alert("test");
-                            var status = $("#status :selected").text();
-                            var address = $("#address").val();
-                            var city = $("#city").val();
-                            var state = $("#state").val();
-                            var zip = $("#zip").val();
-                            var bedrooms = $("#bedrooms").val();
-                            var bathrooms = $("#bathrooms").val();
-                            var price = $("#price").val();
-                            var userId = $("#userId").val();  
-                            $.ajax({
-                                type: "POST",
-                                url: "http://52.11.24.75/Agent/submitHouseInfo.php",
-                                data: {status: status,
-                                      address: address,
-                                      city: city,
-                                      state: state,
-                                      zip: zip,
-                                      bedrooms: bedrooms,
-                                    bathrooms: bathrooms,
-                                    price: price,
-                                    userId: userId}
-                            }); 
-                            window.location.href = "AgentHome.php";
-                        };    
-                    </script>
-            </div>
-                <div style="float:right;">
-                    <div style="text-align: center;">
-                        <object data=http://remax.idxhome.com/homesearch/59157 width="800" height="900"> <embed src=http://remax.idxhome.com/homesearch/59157 width="800" height="900"> </embed> Error: Embedded data could not be displayed. </object>
-                    </div>
-                </div>
-    </body>
-    
+
+<body>
+    <header>
+    </header>
+
+    <form method="post" action="addToDatabase.php">
+    <table>
+        <tr>
+            <td colspan="2"><img align="bottom" alt="Interesting Image" border="0" class="simage float_center" height="151" src="https://d1yoaun8syyxxt.cloudfront.net/dh307-4c1ce6ae-ef18-4d63-ae22-952804c98fc4-v2" style="margin-left: 0px; margin-right: 0px;" title="Interesting Image" width="385" />
+            </td>
+        </tr>
+        <tr>
+          <td>First Name *</td>  <td><input type="text" name="firstName" /> <br /></td>
+        </tr>
+        <tr>
+          <td>Last Name *</td>   <td><input type="text" name="lastName" /> <br /></td>
+        </tr>
+        <tr>
+          <td>Email *</td>       <td><input type="email" name="email" /> <br /></td>
+        </tr>
+        <tr>
+          <td>Phone</td>       <td><input type = "text" name="phone" id="phone" required="" /> <span id="phoneError"></span></td> <br />
+        </tr>
+        <tr>
+          <td>Bedrooms Range</td>       
+                <td>
+                    <input type="number" name="bedroomsMin" min="0" placeholder="min."/>
+                    -
+                    <input type="number" name="bedroomsMax" min="0" placeholder="max"/>
+                </td>  <br />
+        </tr>
+        <tr>
+          <td>Bathrooms Range</td> 
+                <td>
+                    <input type="number" name="bathroomsMin" min="0" placeholder="min." step="0.5" />
+                    -
+                    <input type="number" name="bathroomsMax" min="0" placeholder="max" step="0.5" />
+                </td>  <br />
+        </tr>
+        <tr>
+          <td>Price Range</td>      
+                <td>
+                    <input type="number" name="priceMin" min="0" placeholder="min." step="1000" />
+                    -
+                    <input type="number" name="priceMax" min="0" placeholder="max" step="1000" />
+                </td>  <br />
+        </tr>
+        <input type="hidden" name="houseId" value="<?=$houseId?>"> 
+
+        <tr>
+            <td>
+                <input type="submit" value="Submit" />
+            </td>
+        </tr>
+    </table>
+    </form>
+  
+  <script>
+      $("#phone").change(checkPhone);
+      
+  </script>
+
+</body>
 </html>
