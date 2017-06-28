f594270f5110c02950531b7de78e89554ca17879
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-06-27 20:41:28 -0500

    Debugging table for the inventory

--- agent/agent-active-properties.php
+++ agent/agent-active-properties.php
@@ -292,9 +292,9 @@ if (!isset($_SESSION['userId'])) {
                                                             WHERE userId = :userId
                                                             ORDER BY dateTimes ASC";
                                                    
-                                                $namedParameters = array();
-                                                $namedParameters[':userId'] = $_SESSION['userId'];
-                                                $stmt = $dbConn -> prepare($sql);
+                                                //$namedParameters = array();
+                                                //$namedParameters[':userId'] = $_SESSION['userId'];
+                                                //$stmt = $dbConn -> prepare($sql);
                                                 /*$stmt->execute($namedParameters);
                                                 //$stmt->execute();
                                                 $results = $stmt->fetchAll();
