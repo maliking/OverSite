3cb49f22d6392cef79157bf67ddc3e40e85a1d64
Jose Diaz <jodiaz@csumb.edu>
2017-06-27 21:28:43 -0700

    adding view visitors page to agent side

--- agent/viewVisitors.php
+++ agent/viewVisitors.php
@@ -79,7 +79,7 @@ To change this template use Tools | Templates.
             $dbConn = getConnection();
             $sql = "SELECT * FROM BuyerInfo WHERE :userId = 38";
             $namedParameters = array();
-            $namedParameters[':userId'] = $_GET['userId'];
+            $namedParameters[':userId'] = $_SESSION['userId'];
             $stmt = $dbConn -> prepare($sql);
             $stmt->execute($namedParameters);
             //$stmt->execute();
