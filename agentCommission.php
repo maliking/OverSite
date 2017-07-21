52515c2c4e093dec7311b5bc3a532f6c6a46e36b
Jose Diaz <jodiaz@csumb.edu>
2017-07-20 20:47:45 -0700

    fixing bugs

--- agentCommission.php
+++ agentCommission.php
@@ -4,10 +4,6 @@ $license = $_GET['license'];
 
 require("databaseConnection.php");  
 $dbConn = getConnection();
-if(!isset($_SESSION['userId'])) 
-{
-    header("Location: index.html?error=wrong username or password");
-} 
 
 $sql = "SELECT TYGross, FYGross FROM UsersInfo WHERE license = '".$license."'";
 $stmt = $dbConn -> prepare($sql);
