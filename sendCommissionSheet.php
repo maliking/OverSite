ebfbb56577c7889466b57554d3bce4aaac773d93
Jose Diaz <jodiaz@csumb.edu>
2017-07-23 16:43:59 -0700

    fixing bugs

--- sendCommissionSheet.php
+++ sendCommissionSheet.php
@@ -35,7 +35,7 @@ $pdf->Cell(0,10,'Property Address: ' . $_POST['propertyAddress'],0,1);
 $pdf->Cell(0,10,' ',0,1);
 $pdf->Cell(0,10,'                        Agent Initial Gross Commission: $' . $_POST['InitialGross'],0,1);
 $pdf->Cell(0,10,'                        Remax/Broker Fee: $' . $_POST['brokerFee'],0,1);
-$pdf->Cell(0,10,'                        Agent Subtotal: $' . ($_POST['InitialGross'] - $_POST['brokerFee']),0,1);
+$pdf->Cell(0,10,'                        Agent Subtotal: $' . ($_POST['InitialGross'] . $_POST['brokerFee']),0,1);
 $pdf->Cell(0,10,'                        Processing Fee: $200.00  Flat fee fixed ',0,1);
 $pdf->Cell(0,10,'                        TC. Tech Fee:  $50.00  Flat fee fixed ',0,1);
 $pdf->Cell(0,10,'                        E&O Insurance:  $99.00  Flat fee fixed ',0,1);
