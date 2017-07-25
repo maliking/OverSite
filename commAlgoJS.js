34c8452789f02b8d1eebb60fb12de183ba2aa521
Jose Diaz <jodiaz@csumb.edu>
2017-07-24 22:33:07 -0700

    fixing bugs

--- commAlgoJS.js
+++ commAlgoJS.js
@@ -4,7 +4,7 @@ function calculateCommission(misc)
   var difference = 0;
   var TYGross = document.getElementById("beg-comm").value;
   var commission = document.getElementById("gross-comm").value;
-  console.log(commission);
+  console.log("commission");
   if(TYGross <= 80000)
         {
           difference =  80000 - TYGross;
