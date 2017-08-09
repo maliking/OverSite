b447645a7875112f18e3f1ccfc2cec6b060f9908
Jose Diaz <jodiaz@csumb.edu>
2017-08-09 01:23:27 -0700

    testing view commission sheet

--- viewCommissionSheet.php
+++ viewCommissionSheet.php
@@ -37,19 +37,7 @@ if ($err) {
   echo $response;
 }
 
-$pdf_base64 = $response;
-//Get File content from txt file
-$pdf_base64_handler = fopen($pdf_base64,'r');
-$pdf_content = fread ($pdf_base64_handler,filesize($pdf_base64));
-fclose ($pdf_base64_handler);
-//Decode pdf content
-$pdf_decoded = base64_decode ($pdf_content);
-//Write data back to pdf file
-$pdf = fopen ('test.pdf','w');
-fwrite ($pdf,$pdf_decoded);
-//close output file
-fclose ($pdf);
-echo 'Done';
+
 ?>
 
 <!DOCTYPE html>
