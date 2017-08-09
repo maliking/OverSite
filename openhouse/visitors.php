edaf08953851ee43ef34d8646a1b5f7a332f3f05
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-08-09 14:23:43 -0500

    Making openhouse my visitors be taken from database

--- openhouse/visitors.php
+++ openhouse/visitors.php
@@ -287,13 +287,7 @@
                 echo "<td>" . $result['firstName'] . " " . $result['lastName'] . "</td>";
                 echo "<td>" . $result['phone'] . "</td>";
                 echo "<td>" . htmlspecialchars($result['email']) . "</td>";
-                if($result['houseId'] != 1){
-                    echo "<td>" . htmlspecialchars(getHouseAddress($result['houseId'])) . "</td>";
-                }
-                else {
-                    echo "<td>lol</td>";
-                }       
-
+                echo "<td>" . htmlspecialchars(getHouseAddress($result['houseId'])) . "</td>";
                 echo "<td>
                         <button>Call</button>
                         <button>Text</button>
