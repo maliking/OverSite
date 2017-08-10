58bb99758ae899c9d60d1d5c746057d5fc8617dd
Jose Diaz <jodiaz@csumb.edu>
2017-08-09 19:52:56 -0700

    adding calendar to agent dash

--- agent/index.php
+++ agent/index.php
@@ -605,13 +605,15 @@ if (!isset($_SESSION['userId'])) {
 
     <!-- jQuery 2.2.3 -->
     <script src="../plugins/jQuery/jquery-2.2.3.min.js"></script>
-        <script type="text/javascript"> var original = $.noConflict(true); </script>
+        
 
     <!-- Bootstrap 3.3.6 -->
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 
     <!-- Slimscroll -->
     <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <script type="text/javascript"> var original = $.noConflict(true); </script>
+    
     <!-- FastClick -->
     <script src="../plugins/fastclick/fastclick.js"></script>
     <!-- AdminLTE App -->
