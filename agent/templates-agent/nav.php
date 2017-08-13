7f31875cef59ef6f767135d031487a8a4424f568
Mali King <markiepeanut111@gmail.com>
2017-08-13 15:17:14 -0700

    Fixed nav liink

--- agent/templates-agent/nav.php
+++ agent/templates-agent/nav.php
@@ -10,7 +10,7 @@
             <li class="header">OVERVIEW</li>
             <!-- Optionally, you can add icons to the links -->
             <li><a href="./index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-            <li><a href="#"><i class="fa fa-home"></i> <span>Open House</span></a></li>
+            <li><a href="../openhouse/index.php"><i class="fa fa-home"></i> <span>Open House</span></a></li>
             <li><a href="my-calendar.php"><i class="fa fa-calendar"></i> <span>Calendar</span></a></li>
             <li class="header-style treeview">
                 <a href="#"><span class="header-color">PROPERTIES</span>
