821eb52e32137ec613a93f00fdeebd0292725ea7
Jose Diaz <jodiaz@csumb.edu>
2017-08-13 23:05:54 -0700

    changing getting agent TYcommission

--- agentCommission.php
+++ agentCommission.php
@@ -6,7 +6,7 @@ $license = $_GET['license'];
 require("databaseConnection.php");  
 $dbConn = getConnection();
 
-$sql = "SELECT TYGross, FYGross FROM commInfo WHERE license = '".$license."'";
+$sql = "SELECT TYGross, FYGross FROM commInfo WHERE license = '".$license."' ORDER BY TYGross DESC";
 $stmt = $dbConn -> prepare($sql);
 $stmt->execute();
 $userResults = $stmt->fetch();
