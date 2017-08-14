a716d4ac6e16f8516003347e83d9664563561fd6
Jose Diaz <jodiaz@csumb.edu>
2017-08-13 23:51:02 -0700

    testing adding envelopeId

--- sendCommissionSheet.php
+++ sendCommissionSheet.php
@@ -134,15 +134,15 @@ $pdf->Cell(0,10,'Agent Signature                              Date              
     "x-docusign-authentication: { \"Username\": \"" . $username . "\",\"Password\":\"" . $password ."\",\"IntegratorKey\":\"" . $intKey . "\" }"
   	),
 	));
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
-	curl_close($curl);
-		if ($err) {
-	  echo "cURL Error #:" . $err;
-	} else {
-		echo $response;
+	// $response = curl_exec($curl);
+	// $err = curl_error($curl);
+	// curl_close($curl);
+	// 	if ($err) {
+	//   echo "cURL Error #:" . $err;
+	// } else {
+	// 	echo $response;
 	  	
-	}
+	// }
 	
 	// $envId = json_decode($response, true);
 	// $namedParameters[":envelopeId"] = $envId['envelopeId'];
