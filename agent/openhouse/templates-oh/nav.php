08be9987ce05bf926c03080168348e8117db6a72
Mali King <markiepeanut111@gmail.com>
2017-08-14 17:35:21 -0700

    Fixed broken link in nv

--- agent/openhouse/templates-oh/nav.php
+++ agent/openhouse/templates-oh/nav.php
@@ -11,7 +11,7 @@
             <li class="active"><a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
             <li><a href="listings-openhouse.php"><i class="fa fa-home"></i> <span>Open House</span></a></li>
             <li><a href="visitors.php"><i class="fa fa-male"></i> <span>My Visitors</span></a></li>
-            <li><a href="#" target="_blank"><i class="fa fa-edit"></i> <span>Sign In Sheet</span></a></li>
+            <li><a href="../signIn.php" target="_blank"><i class="fa fa-edit"></i> <span>Sign In Sheet</span></a></li>
             <li><a href="#"><i class="fa fa-globe"></i> <span>Geofencing</span></a></li>
         </ul>
         <!-- /.sidebar-menu -->
