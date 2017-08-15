674716492580b8692ff74d8dc9bd048fe99b804e
erinengle21 <eengle@csumb.edi>
2017-08-15 01:43:06 -0700

    added notifications to agent dashboard

--- agent/index.php
+++ agent/index.php
@@ -1,9 +1,9 @@
 <?php
-//session_start();
-//
-//if (!isset($_SESSION['userId'])) {
-//    header("Location: http://jjp17.org/login.php");
-//}
+session_start();
+
+if (!isset($_SESSION['userId'])) {
+    header("Location: http://jjp17.org/login.php");
+}
 ?>
 
     <!DOCTYPE html>
