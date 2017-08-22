a84c05398b01da47e18ea1419dfd4fb3e5f00dbe
Jose Diaz <jodiaz@csumb.edu>
2017-08-21 21:17:42 -0700

    fixed url in header file

--- templates-admin/header.php
+++ templates-admin/header.php
@@ -110,7 +110,7 @@
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left"> <a href="#" class="btn btn-default btn-flat">Profile</a> </div>
-                            <div class="pull-right"> <a href="../logout.php" class="btn btn-default btn-flat">Sign out</a> </div>
+                            <div class="pull-right"> <a href="logout.php" class="btn btn-default btn-flat">Sign out</a> </div>
                         </li>
                     </ul>
                 </li>
