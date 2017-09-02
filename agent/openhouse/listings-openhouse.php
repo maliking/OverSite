acb3ff34b3763dff1b5dad20d1f5f48efcc4118b
Jose Diaz <jodiaz@csumb.edu>
2017-09-01 22:25:44 -0700

    added urls to the options menu of each house

--- agent/openhouse/listings-openhouse.php
+++ agent/openhouse/listings-openhouse.php
@@ -281,8 +281,8 @@ $url = 'https://api.idxbroker.com/clients/featured';
                                                         </button>
                                                         <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu1\">";
             echo "                                      <li><a href=\"create-flyer.php?id=" . $response[$keys[$i]]['listingID']  ."\">Create a New Flyer</a></li>";
-            echo "                                          <li><a href=\"#\">Flyer Info</a></li>
-                                                            <li><a href=\"#\">Sign-In</a></li>
+            echo "                                          <li><a href=\"listing-info.php?id=" .$response[$keys[$i]]['listingID']  ."\">Flyer Info</a></li>
+                                                            <li><a href=\"../signIn.php?id=" . $response[$keys[$i]]['listingID']  ." \">Sign-In</a></li>
                                                             <li role=\"separator\" class=\"divider\"></li>
                                                             <li><a href=\"#\">Remove</a></li>
                                                         </ul>
