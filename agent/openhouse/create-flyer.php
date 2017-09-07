f3a7aac53d683ddd0de567e1370711de14c73d87
Jose Diaz <jodiaz@csumb.edu>
2017-09-06 23:13:33 -0700

    fixing bug

--- agent/openhouse/create-flyer.php
+++ agent/openhouse/create-flyer.php
@@ -354,7 +354,7 @@ $keys = array_keys($response);
                     // alert(allVals[2]);
                     $.post( "generatePDF.php", { imageOne: allVals[0], imageTwo: allVals[1], imageThree: allVals[2], imageFour: allVals[3], imageFive: allVals[4],
                      bedrooms:flyerContent[0], bathrooms: flyerContent[1], sqft: flyerContent[2], mlsId:flyerContent[3], address: addressC, city: cityC, 
-                     state: stateC, zip: zipC, price: priceC, description: description } );
+                     state: stateC, zip: zipC, price: priceC, description: description } )
                     .done(function( data ) {
                         alert( "Data Loaded: ");
                       });
