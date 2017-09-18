c8bd4af4fea709006e3fa55ea01e2c204503484f
Jose Diaz <jodiaz@csumb.edu>
2017-09-18 11:15:21 -0700

    fixing urlencode

--- agent/addBuyer.php
+++ agent/addBuyer.php
@@ -51,7 +51,7 @@ $twilio_phone_number = "+18315851661";
         array(
         "From" => $twilio_phone_number,
         "Body" => "Flyer",
-        'mediaUrl' => urlencode("http://52.11.24.75/uploadFlyers/" . $_SESSION['flyer']),
+        'mediaUrl' => "http://52.11.24.75/uploadFlyers/" . urlencode($_SESSION['flyer']),
         )
     );
     // }
