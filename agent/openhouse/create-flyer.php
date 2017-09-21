39742326ebb244459da67a6874ced95bc35f2f94
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-09-21 06:55:41 -0500

    Debugging image order

--- agent/openhouse/create-flyer.php
+++ agent/openhouse/create-flyer.php
@@ -372,6 +372,10 @@ $keys = array_keys($response);
                 var str = $ch.filter(":checked").map(function(i, el) {
                     return el.value;
                 }).get().join("\n");
+
+                $.each(str, function( k, v ) {
+                  alert( "Key: " + k + ", Value: " + v );
+                });
                 $("#result").text(str).show();
             });
 /*
