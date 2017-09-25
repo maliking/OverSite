92adf66c65972a8aa1b484f08010c290adca4717
erinengle21 <eengle@csumb.edi>
2017-09-24 20:32:32 -0700

    additional links

--- agent/index.php
+++ agent/index.php
@@ -1,9 +1,9 @@
 <?php
-//session_start();
-//
-//if (!isset($_SESSION['userId'])) {
-//    header("Location: http://jjp2017.org/login.php");
-//}
+session_start();
+
+if (!isset($_SESSION['userId'])) {
+    header("Location: http://jjp2017.org/login.php");
+}
 require '../databaseConnection.php';
 $dbConn = getConnection();
 
