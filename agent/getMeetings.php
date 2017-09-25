03565047735490ae41786d7edea77e3714734d9f
Jose Diaz <jodiaz@csumb.edu>
2017-09-24 21:22:17 -0700

    Adding code to populate full calendar

---
+++ agent/getMeetings.php
@@ -0,0 +1,24 @@
+<?php
+
+// SELECT * FROM `BuyerInfo` WHERE `meeting` = "0000-00-00 00:00:00"   Select all that dont have meeting time or date
+
+// SELECT * FROM `BuyerInfo` WHERE MONTH(`registeredDate`) = 9     Select all from current month
+
+// date("d-M-Y", strtotime("next monday")); gets next monday
+
+session_start();
+require '../databaseConnection.php';
+$dbConn = getConnection();
+
+$monthMeetings = "SELECT * FROM BuyerInfo WHERE MONTH(registeredDate) >= :registeredDate";
+
+$namedParameters = array();
+$namedParameters[':registeredDate'] = date('m');
+
+$meetingStmt = $dbConn->prepare($monthMeetings);
+$meetingStmt->execute($namedParameters);
+$meetingResults = $meetingStmt->fetchAll();
+
+
+
+?>
