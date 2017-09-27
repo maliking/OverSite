e36102f23dd01fcb7023be1f6e07cb8231515faa
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-09-27 17:44:27 -0500

    Unselecting image from top bar

--- agent/openhouse/create-flyer.php
+++ agent/openhouse/create-flyer.php
@@ -406,7 +406,7 @@ $keys = array_keys($response);
                 jQuery.each($ch.filter(":checked"), function(k, v) {
                     if(itemToRemove === v.value){
                         $ch.splice($.inArray(itemToRemove, $ch),1);
-                        $("input[type='checkbox'][value='"+itemToRemove+"']").prop('unchecked',true); //NEED TO UNCHECK BOX
+                        $("input[type='checkbox'][value='"+itemToRemove+"']").prop('checked',false); //NEED TO UNCHECK BOX
                         return false;
                     }
                 });
