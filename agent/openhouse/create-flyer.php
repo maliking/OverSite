0fb27bb41f651fd3b1187a80103614895f2b0529
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-09-27 07:44:48 -0500

    Unselecting image from top bar

--- agent/openhouse/create-flyer.php
+++ agent/openhouse/create-flyer.php
@@ -399,7 +399,7 @@ $keys = array_keys($response);
             });
 
             $('#selectedImages').click(function(){
-                alert(this.attr('src'));
+                alert($(this).attr('src'));
             });
 
         </script>
