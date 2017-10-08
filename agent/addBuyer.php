63124433bf61fc5f690e5fea33e725f0c0b4f6cd
Jose Diaz <jodiaz@csumb.edu>
2017-10-07 18:39:17 -0700

    fixed bugs

--- agent/addBuyer.php
+++ agent/addBuyer.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
+// ini_set('display_errors', 'On');
 session_start();
 date_default_timezone_set('America/Los_Angeles');
 require '../databaseConnection.php';
