66f2047bf5bf28d38d022e8ec2b1807eb6bd3158
Jose Diaz <jodiaz@csumb.edu>
2017-10-10 22:32:36 -0700

    fixing modal

--- agent/openhouse/create-flyer.php
+++ agent/openhouse/create-flyer.php
@@ -179,14 +179,11 @@ $keys = array_keys($response);
 
                         <!-- Modal content-->
                         <div class="modal-content">
-                          <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Message Sent</h4>
+                          
+                          <div class="modal-body">
+                            <p>Message Sent</p>
                           </div>
                           
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                          </div>
                         </div>
 
                       </div>
