ce1bbcae74cc290f6875b1cb9f9474cb2193d2c1
erinengle21 <eengle@csumb.edi>
2017-10-23 23:16:40 -0700

    changed profile option to settings

--- agent/templates-agent/header.php
+++ agent/templates-agent/header.php
@@ -108,18 +108,10 @@
                                 <small>Member since Nov. 2012</small>
                             </p>
                         </li>
-                        <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="row">
-                                <div class="col-xs-4 text-center"><a href="#">Followers</a></div>
-                                <div class="col-xs-4 text-center"><a href="#">Sales</a></div>
-                                <div class="col-xs-4 text-center"><a href="#">Friends</a></div>
-                            </div>
-                            <!-- /.row -->
-                        </li>
+                       
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <div class="pull-left"><a href="#" class="btn btn-default btn-flat">Profile</a></div>
+                            <div class="pull-left"><a href="settings.php" class="btn btn-default btn-flat">Settings</a></div>
                             <div class="pull-right"><a href="../logout.php" class="btn btn-default btn-flat">Sign
                                     out</a></div>
                         </li>
