b0bba977e15987e1036f4b233b96fac8cdd89e66
Jose Diaz <jodiaz@csumb.edu>
2017-10-27 19:40:12 -0700

    adding code to display meetings according to agent

--- agent/getMeetings.php
+++ agent/getMeetings.php
@@ -10,11 +10,12 @@ session_start();
 require '../databaseConnection.php';
 $dbConn = getConnection();
 
-$monthMeetings = "SELECT CONCAT(firstName, ',', lastName) as title , DATE_FORMAT(meeting, '%Y-%m-%dT%H:%i:%s') as start, ADDTIME(meeting, '00:15:00') as end, DATE_FORMAT(meeting, '%Y-%m-%dT%H:%i:%s') as id  FROM BuyerInfo WHERE MONTH(registeredDate) >=:registeredDate";
+// Add WHERE clause to get specific agents - WHERE userId = :userId  $_SESSION['userId'];
+$monthMeetings = "SELECT CONCAT(firstName, ',', lastName) as title , DATE_FORMAT(meeting, '%Y-%m-%dT%H:%i:%s') as start, ADDTIME(meeting, '00:15:00') as end, DATE_FORMAT(meeting, '%Y-%m-%dT%H:%i:%s') as id  FROM BuyerInfo WHERE MONTH(registeredDate) >=:registeredDate AND userId = :userId";
 
 $namedParameters = array();
 $namedParameters[':registeredDate'] = date('m');
-
+$namedParameters[':userId'] = $_SESSION['userId'];
 $meetingStmt = $dbConn->prepare($monthMeetings);
 $meetingStmt->execute($namedParameters);
 $meetingResults = $meetingStmt->fetchAll();
