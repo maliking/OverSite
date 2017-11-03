90fb733f9e88f577dfc3b9aedf5696fa53dbaa99
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2017-11-03 13:35:51 -0600

    Debugging check username

--- getAgentRosterRows.php
+++ getAgentRosterRows.php
@@ -4,7 +4,7 @@ session_start();
 require 'databaseConnection.php';
 
 $dbConn = getConnection();
-$sql = "SELECT * FROM  UsersInfo WHERE userType = 0 AND userId != :userId";
+$sql = "SELECT * FROM  UsersInfo WHERE userType = 1 AND userId != :userId";
 $stmt = $dbConn->prepare($sql);
 $stmt->execute();
 $result = $stmt->fetchAll();
