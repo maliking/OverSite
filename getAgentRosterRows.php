6d92259d50645b6e3aaada84b0ec900e29a9b44e
Jose Diaz <jodiaz@csumb.edu>
2017-11-03 13:25:01 -0700

    fixing bugs

--- getAgentRosterRows.php
+++ getAgentRosterRows.php
@@ -4,9 +4,11 @@ session_start();
 require 'databaseConnection.php';
 
 $dbConn = getConnection();
-$sql = "SELECT * FROM  UsersInfo WHERE userType = 1 AND userId != :userId";
+$sql = "SELECT * FROM  UsersInfo WHERE userType = '1' AND userId != :userId";
+$namedParameters = array();
+$namedParameters[':userId'] = $_SESSION['userId'];
 $stmt = $dbConn->prepare($sql);
-$stmt->execute();
+$stmt->execute($namedParameters);
 $result = $stmt->fetchAll();
 
 echo json_encode($result);
