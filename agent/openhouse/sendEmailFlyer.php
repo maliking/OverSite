e81b3926b4ee5988e86df4602aad5c0632369e63
Jose Diaz <jodiaz@csumb.edu>
2017-11-07 21:17:43 -0800

    fixing email bugs

--- agent/openhouse/sendEmailFlyer.php
+++ agent/openhouse/sendEmailFlyer.php
@@ -9,7 +9,7 @@ $mail->setFrom('jodiaz@csumb.edu', 'Your Name');
 $mail->addAddress($_POST['email'], 'My Friend');
 $mail->Subject  =  "Flyer from house";
 $mail->Body     = $_POST['flyerMessage'] ;
-$mail->addAttachment("../../uploadFlyers/" . $_POST['flyer'] . 'jpg')
+// $mail->addAttachment("../../uploadFlyers/" . $_POST['flyer'] . 'jpg')
 if(!$mail->send()) {
   echo 'Message was not sent.';
   echo 'Mailer error: ' . $mail->ErrorInfo;
