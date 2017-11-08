c8601fb577480a66f6d55206b464e25d4561b6b4
Jose Diaz <jodiaz@csumb.edu>
2017-11-07 21:41:37 -0800

    fixing email bugs

--- agent/openhouse/sendEmailFlyer.php
+++ agent/openhouse/sendEmailFlyer.php
@@ -13,7 +13,7 @@ $mail->addAddress($_POST['email'], 'My Friend');
 $mail->Subject  =  "Flyer from house";
 $mail->Body     = $_POST['flyerMessage'] ;
 // $mail->addAttachment("../../uploadFlyers/" . rawurlencode($_POST['flyer']) . 'jpg')
-$mail->addStringAttachment(file_get_contents("../../uploadFlyers/" . rawurlencode($_POST['flyer']) . 'jpg'), $_POST['flyer']. 'jpg');
+$mail->addStringAttachment(file_get_contents("http://52.11.24.75/uploadFlyers/" . rawurlencode($_POST['flyer']) . 'jpg'), $_POST['flyer']. 'jpg');
 if(!$mail->send()) {
   echo 'Message was not sent.';
   echo 'Mailer error: ' . $mail->ErrorInfo;
