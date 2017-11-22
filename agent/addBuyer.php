6cbd6d9e801aaf58e3d619f5bf727ec8aaa4b8e4
Jose Diaz <jodiaz@csumb.edu>
2017-11-21 23:33:25 -0800

    fixing bugs

--- agent/addBuyer.php
+++ agent/addBuyer.php
@@ -16,35 +16,35 @@ use PHPMailer\PHPMailer\Exception;
 use Twilio\Rest\Client;
 
 
-// $url = 'https://api.idxbroker.com/clients/featured';
+$url = 'https://api.idxbroker.com/clients/featured';
 
 
-// // headers (required and optional)
-// $headers = array(
-//     'Content-Type: application/x-www-form-urlencoded', // required
-//     'accesskey: e1Br0B5DcgaZ3@JXI9qib5', // required - replace with your own
-//     'outputtype: json' // optional - overrides the preferences in our API control page
-// );
+// headers (required and optional)
+$headers = array(
+    'Content-Type: application/x-www-form-urlencoded', // required
+    'accesskey: e1Br0B5DcgaZ3@JXI9qib5', // required - replace with your own
+    'outputtype: json' // optional - overrides the preferences in our API control page
+);
 
-// $handle = curl_init();
-// curl_setopt($handle, CURLOPT_URL, $url);
-// curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
-// curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+$handle = curl_init();
+curl_setopt($handle, CURLOPT_URL, $url);
+curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
 
-// // exec the cURL request and returned information. Store the returned HTTP code in $code for later reference
-// $response = curl_exec($handle);
-// $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+// exec the cURL request and returned information. Store the returned HTTP code in $code for later reference
+$response = curl_exec($handle);
+$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-// if ($code >= 200 || $code < 300) {
-//     $response = json_decode($response, true);
-// } else {
-//     $error = $code;
-// }
+if ($code >= 200 || $code < 300) {
+    $response = json_decode($response, true);
+} else {
+    $error = $code;
+}
 
 
-// $keys = array_keys($response);
+$keys = array_keys($response);
 
 
 // session_start();
@@ -128,7 +128,7 @@ $namedParameters[':howSoon'] = $_POST['howSoon'];
 $stmt = $dbConn->prepare($sql);
 try {
     $stmt->execute($namedParameters);
-    // $lastBuyerId = $stmt->lastInsertId();
+    $lastBuyerId = $stmt->lastInsertId();
 } catch (Exception $e) {
     echo 'Caught exception: ', $e->getMessage(), "\n";
 
