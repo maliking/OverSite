37423786d18fa86aec80760818154fa5508cd3ea
Jose Diaz <jodiaz@csumb.edu>
2017-11-21 23:55:37 -0800

    fixing bugs

--- agent/addBuyer.php
+++ agent/addBuyer.php
@@ -251,21 +251,21 @@ for ($i = 0; $i < sizeof($keys); $i++)
 
 }
 
-// if($match > 1)
-// {
-    // $twilio_phone_number = "+18315851661";
-    // // if($houseId == "89")
-    // // {
-    // $client = new Client($sid, $token);
-    // $client->messages->create(
-    //     //$result['phone']
-    //     "8312934153",
-    //     array(
-    //         "From" => $twilio_phone_number,
-    //         "Body" => $messageForLeadAgent,
-    //     )
-    // );
-// }
+if($match > 1)
+{
+    $twilio_phone_number = "+18315851661";
+    // if($houseId == "89")
+    // {
+    $client = new Client($sid, $token);
+    $client->messages->create(
+        //$result['phone']
+        "8312934153",
+        array(
+            "From" => $twilio_phone_number,
+            "Body" => $messageForLeadAgent,
+        )
+    );
+}
 
 
 // }
