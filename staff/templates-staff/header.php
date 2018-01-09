d50f519123b7b3b1f5a77bb64f79c6fd9a948679
erinengle21 <eengle@csumb.edi>
2018-01-08 19:42:29 -0800

    removing notification icons

--- staff/templates-staff/header.php
+++ staff/templates-staff/header.php
@@ -16,91 +16,7 @@ $agentInfo = $email->fetch();
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <!-- Messages: style is in dropdown.less-->
-                <li class="dropdown messages-menu">
-                    <!-- Menu toggle button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-envelope-o"></i> <span
-                                class="label label-success">4</span> </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">You have 4 messages</li>
-                        <li>
-                            <!-- Inner message menu -->
-                            <ul class="menu">
-                                <li>
-                                    <!-- Start message -->
-                                    <a href="#">
-                                        <div class="pull-left">
-                                            <!-- User Image --><img src="../dist/img/user2-160x160.jpg"
-                                                                    class="img-circle" alt="User Image"></div>
-                                        <!-- Message title and timestamp -->
-                                        <h4>
-                                            Support Team
-                                            <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                                        </h4>
-                                        <!-- Message content -->
-                                        <p>Why not buy a new awesome theme?</p>
-                                    </a>
-                                </li>
-                                <!-- End message -->
-                            </ul>
-                            <!-- /.menu -->
-                        </li>
-                        <li class="footer"><a href="#">See All Messages</a></li>
-                    </ul>
-                </li>
-                <!-- /.messages-menu -->
-                <!-- Notifications Menu -->
-                <li class="dropdown notifications-menu">
-                    <!-- Menu toggle button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-bell-o"></i> <span
-                                class="label label-warning">10</span> </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">You have 10 notifications</li>
-                        <li>
-                            <!-- Inner nofications menu -->
-                            <ul class="menu">
-                                <li>
-                                    <!-- Start notification -->
-                                    <a href="#"> <i class="fa fa-users text-aqua"></i> 5 new members joined today </a>
-                                </li>
-                                <!-- End notification -->
-                            </ul>
-                        </li>
-                        <li class="footer"><a href="#">View all</a></li>
-                    </ul>
-                </li>
-                <!-- Tasks Menu -->
-                <li class="dropdown tasks-menu">
-                    <!-- Menu Toggle Button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-flag-o"></i> <span
-                                class="label label-danger">9</span> </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">You have 9 tasks</li>
-                        <li>
-                            <!-- Inner tasks menu -->
-                            <ul class="menu">
-                                <li>
-                                    <!-- Task item -->
-                                    <a href="#">
-                                        <!-- Task title and progress text -->
-                                        <h3>
-                                            Design some buttons
-                                            <small class="pull-right">20%</small>
-                                        </h3>
-                                        <!-- Progress bar -->
-                                        <div class="progress xs">
-                                            <div class="progress-bar progress-bar-aqua" style="width: 20%"
-                                                 role="progressbar" aria-valuenow="20" aria-valuemin="0"
-                                                 aria-valuemax="100"><span class="sr-only">20% Complete</span></div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <!-- End task item -->
-                            </ul>
-                        </li>
-                        <li class="footer"><a href="#">View all tasks</a></li>
-                    </ul>
-                </li>
+               
                 <!-- User Account Menu -->
                 <li class="dropdown user user-menu">
                     <!-- Menu Toggle Button -->
@@ -116,14 +32,7 @@ $agentInfo = $email->fetch();
                             </p>
                         </li>
                         <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="row">
-                                <div class="col-xs-4 text-center"><a href="#">Followers</a></div>
-                                <div class="col-xs-4 text-center"><a href="#">Sales</a></div>
-                                <div class="col-xs-4 text-center"><a href="#">Friends</a></div>
-                            </div>
-                            <!-- /.row -->
-                        </li>
+                        
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left"><a href="#" class="btn btn-default btn-flat">Profile</a></div>
