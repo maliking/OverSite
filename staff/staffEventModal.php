c0b6c319487b915278183d1acd02890b97d6c7f9
erinengle21 <eengle@csumb.edi>
2018-01-08 19:34:19 -0800

    fixing spelling

--- staff/staffEventModal.php
+++ staff/staffEventModal.php
@@ -81,7 +81,7 @@
 
           
             <div class="modal-footer">
-                <button type="button" class="btn btn-defualt pull-left" data-dismiss="modal">Close
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close
                                 </button>
                 <button type="button" class="btn btn-default">Save 
                                 </button>
