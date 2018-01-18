f517a704e651551301d97a119c348a8e8080fd67
Jose Diaz <jodiaz@csumb.edu>
2018-01-18 01:25:26 -0800

    testing

--- staff/sendText.php
+++ staff/sendText.php
@@ -3,20 +3,23 @@
 require '../keys/cred.php';
 require '../twilio-php-master/Twilio/autoload.php';
 
-use Twilio\Rest\Client;
+// use Twilio\Rest\Client;
 
 
 // $twilio_phone_number = "+18315851661";
-$twilio_phone_number = "Oversite";
-$client = new Client($sid, $token);
-$client->messages->create(
-$_POST['phone'],
-array(
-    "From" => $twilio_phone_number,
-    "Body" => $_POST['text'],
+// $twilio_phone_number = "Oversite";
+// $client = new Client($sid, $token);
+// $client->messages->create(
+// $_POST['phone'],
+// array(
+//     "From" => $twilio_phone_number,
+//     "Body" => $_POST['text'],
     
-    )
-);
+//     )
+// );
 	
+use Twilio\Twiml;
 
+$response = new Twiml();
+$response->message($_POST['text']);
 ?>
