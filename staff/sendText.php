2cf87636cfa4153b2c54ddc244d6b1ea7cb7155e
Jose Diaz <jodiaz@csumb.edu>
2018-01-18 02:32:50 -0800

    testing

--- staff/sendText.php
+++ staff/sendText.php
@@ -6,14 +6,13 @@ require '../twilio-php-master/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
 
-// $twilio_phone_number = "+18315851661";
-$twilio_phone_number = "Oversite";
+$twilio_phone_number = "+18315851661";
+// $twilio_phone_number = "Oversite";
 $client = new Client($sid, $token);
 $client->messages->create(
 $_POST['phone'],
 array(
 	"from" => $twilio_phone_number,
-    "MessagingServiceSid" => $serviceSid,
     "Body" => $_POST['text'],
     
     )
