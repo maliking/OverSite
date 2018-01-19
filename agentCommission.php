05aef3026bb570bc82c56af1388d7e52b334fde3
Jose Diaz <jodiaz@csumb.edu>
2018-01-18 23:29:46 -0800

    getting commission from new year

--- agentCommission.php
+++ agentCommission.php
@@ -6,7 +6,7 @@ $license = $_GET['license'];
 require("databaseConnection.php");
 $dbConn = getConnection();
 
-$sql = "SELECT SUM(TYGross), SUM(FYGross) FROM commInfo WHERE license = '" . $license . "' AND YEAR(date) = :year";
+$sql = "SELECT SUM(TYGross) as TYGross, SUM(FYGross) as FYGross FROM commInfo WHERE license = '" . $license . "' AND YEAR(date) = :year";
 $namedParameters = array();
 $namedParameters[':year'] = date("Y");
 $stmt = $dbConn->prepare($sql);
