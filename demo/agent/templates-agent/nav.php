37f1c3b9acfcb17d0bbcb71fca86cd7c83f2aa0d
Jose Diaz <jodiaz@csumb.edu>
2018-04-03 19:32:22 -0700

    changing logo url

--- demo/agent/templates-agent/nav.php
+++ demo/agent/templates-agent/nav.php
@@ -4,7 +4,7 @@
     <section class="sidebar">
         <a href="index.php" class="logo">
             <!-- Logo -->
-            <img class="img-responsive" src="../dist/img/houseLogo.png"> </a>
+            <img class="img-responsive" src="../dist/img/houseLogo.jpeg"> </a>
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">OVERVIEW</li>
