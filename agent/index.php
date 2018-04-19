5cee7677c74743d8bd331f44d4be617ecfc5992b
Jose Diaz <jodiaz@csumb.edu>
2018-04-18 19:22:47 -0700

    design agent boxes

--- agent/index.php
+++ agent/index.php
@@ -182,7 +182,7 @@ $keys = array_keys($response);
                             <!-- small box -->
                             <div class="small-box bg-blue">
                                 <div class="inner">
-                                    <p style="font-size:20px; border-bottom: 2px solid black"><b>Closed Units Rank</b></p>
+                                    <p style="font-size:20px; border-style: 2px solid black; background-color:#000080;"><b>Closed Units Rank</b></p>
                                         <?php 
 
                                         if($soldRank == 0)
