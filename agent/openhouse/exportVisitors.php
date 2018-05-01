b8f7109606b0306f1bbe65e1bcc4520b221905eb
Jose Diaz <jodiaz@csumb.edu>
2018-05-01 00:34:33 -0700

    changing export leads

--- agent/openhouse/exportVisitors.php
+++ agent/openhouse/exportVisitors.php
@@ -56,23 +56,107 @@ $visitors = $stmtVisitors->fetchAll();
 $today = date("m/d/Y");
 
 
-echo 'FirstName' . "\t" . 'LastName' . "\t" . 'Title' . "\t" . 'Company' . "\t" . 'Address1' . "\t" .
-    'Address2' . "\t" . 'City' . "\t" . 'State' . "\t" . 'Zip' . "\t" . 'Country' . "\t" . 'County' . "\t" . 'BizPhone' . "\t" .
-    'HomePhone' . "\t" . 'CellPhone' . "\t" . 'Fax' . "\t" . 'Email' . "\t" . 'WebURL' . "\t" . 'Active' . "\t" .
-    'LeadPriority' . "\t" . 'AssignedTo' . "\t" . 'LeadSource' . "\t" . 'AdCode' . "\t" . 'AddedOn' . "\t" . 'ModifiedOn' . "\t" . 'Office' . "\t" . 'ContactTypes' . "\t" .
-    'Anniversary' . "\t" . 'Birthday' . "\t" . 'BizPhone2' . "\t" . 'BizPhone2Ext' . "\t" . 'CellPhone2' . "\t" . 'ClosingDate' . "\t" . 'CommissionEstimate' .
-    "\t" . 'Email2' . "\t" . 'ReferredBy' . "\t" . 'SpouseBizPhone' . "\t" . 'SpouseBizPhoneExt' . "\t" . 'SpouseCellPhone' . "\t" . 'SpouseEmail' . "\t" . 'SpouseFirstName' .
-    "\t" . 'SpouseLastName' . "\t" . 'WorkAddress' . "\t" . 'WorkCity' . "\t" . 'WorkCountry' . "\t" . 'WorkCounty' . "\t" . 'WorkState' . "\t" . 'WorkZip' . "\n";
+echo 'Title' . "\t" . 
+    'FirstName' . "\t" . 
+    'LastName' . "\t" .  
+    
+    'Address' . "\t" .
+    // 'Address1' . "\t" .
+    // 'Address2' . "\t" . 
+    'City' . "\t" . 
+    'County' . "\t" .
+    'State' . "\t" . 
+    'Zip' . "\t" . 
+    'Country' . "\t" .
+    'EmailAddress' . "\t" .
+    'HomePhone' . "\t" . 
+    'CellPhone' . "\t" .  
+    'BusinessPhone' . "\t" . 
+    'Fax' . "\t" .  
+    'Company' . "\t" . 
+    'Notes' . "\t" .
+    'WebURL' . "\n"; 
+    // 'Active' . "\t" .
+    // 'LeadPriority' . "\t" . 
+    // 'AssignedTo' . "\t" . 
+    // 'LeadSource' . "\t" . 
+    // 'AdCode' . "\t" . 
+    // 'AddedOn' . "\t" . 
+    // 'ModifiedOn' . "\t" . 
+    // 'Office' . "\t" . 
+    // 'ContactTypes' . "\t" .
+    // 'Anniversary' . "\t" . 
+    // 'Birthday' . "\t" . 
+    // 'BizPhone2' . "\t" . 
+    // 'BizPhone2Ext' . "\t" . 
+    // 'CellPhone2' . "\t" . 
+    // 'ClosingDate' . "\t" . 
+    // 'CommissionEstimate' . "\t" . 
+    // 'Email2' . "\t" . 
+    // 'ReferredBy' . "\t" . 
+    // 'SpouseBizPhone' . "\t" . 
+    // 'SpouseBizPhoneExt' . "\t" . 
+    // 'SpouseCellPhone' . "\t" . 
+    // 'SpouseEmail' . "\t" . 
+    // 'SpouseFirstName' . "\t" . 
+    // 'SpouseLastName' . "\t" . 
+    // 'WorkAddress' . "\t" . 
+    // 'WorkCity' . "\t" . 
+    // 'WorkCountry' . "\t" . 
+    // 'WorkCounty' . "\t" . 
+    // 'WorkState' . "\t" . 
+    // 'WorkZip' . "\n";
 
 foreach ($visitors as $visit) {
     # code...
 
-    echo $visit['firstName'] . "\t" . $visit['lastName'] . "\t" . ' ' . "\t" . ' ' . "\t" . $visit['address'] . ", " . $visit['city'] . " " . $visit['state'] . 
-        " " . $visit['zip']  . "\t" . ' ' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . $visit['phone'] . "\t" . '' . "\t" . '' . "\t" .
-        '' . "\t" . $visit['email'] . "\t" . '' . "\t" . 'TRUE' . "\t" . '' . "\t" . 'Jorge Edeza' . "\t" . 'Manually Entered' . "\t" . '' . "\t" .
-        $today . "\t" . $today . "\t" . 'RE/MAX Property Experts' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' .
-        "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' . "\t" . '' .
-        "\t" . '' . "\t" . '' . "\t" . '' . "\n";
+    echo ' ' . "\t" .
+         $visit['firstName'] . "\t" . 
+         $visit['lastName'] . "\t" .  
+         $visit['address'] .  ", " . $visit['city'] . " " . $visit['state'] . " " . $visit['zip']  . "\t" . 
+         // ' ' . "\t" . 
+         $visit['city'] . "\t" . 
+         '' . "\t" . 
+         $visit['state'] . "\t" . 
+         $visit['zip'] . "\t" . 
+         '' . "\t" . 
+         $visit['email'] . "\t" .
+         $visit['phone'] . "\t" . 
+         '' . "\t" . 
+         '' . "\t" .
+         '' . "\t" .  
+         '' . "\t" . 
+         ' ' . "\t" . 
+         // 'TRUE' . "\t" . 
+         '' . "\n" ; 
+        //  'Jorge Edeza' . "\t" . 
+        //  'Manually Entered' . "\t" . 
+        //  '' . "\t" .
+        // $today . "\t" . 
+        // $today . "\t" . 
+        // 'RE/MAX Property Experts' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\t" . 
+        // '' . "\n";
 
 }
 
