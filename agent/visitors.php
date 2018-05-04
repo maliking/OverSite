1032b7de5127a3079222b19dd1d444b877f94819
Jose Diaz <jodiaz@csumb.edu>
2018-05-04 00:05:37 -0700

    testing

--- agent/visitors.php
+++ agent/visitors.php
@@ -410,7 +410,7 @@ for ($h = 0; $h < sizeof($keys); $h++)
                             <div class="clearfix"></div>
                         </div>
 
-                        <div class="box-body" style="height: 100%; overflow: auto;">
+                        <div class="box-body" style="height: 600px; overflow: auto;">
                             <table class="table table-striped" data-filtering="true">
                                 <thead>
                                 <tr>
