f0a4e679ff111cd93392091813e7f230591974e2
Jose Diaz <jodiaz@csumb.edu>
2018-06-02 00:29:58 -0700

    adding osa portal

---
+++ osa/index.php
@@ -0,0 +1,208 @@
+<?php
+session_start();
+// echo($_SESSION['userType']);
+// if (!isset($_SESSION['userId'])) {
+//     header("Location: ../login.php");
+// }
+
+// if ($_SESSION['userType'] == "0") {
+//     header("Location: ../index.php");
+// }
+
+// if ($_SESSION['userType'] == "1") {
+//     header("Location: ../agent/index.php");
+// }
+
+require '../databaseConnection.php';
+
+$dbConn = getConnection();
+
+$sqlGetVisitors = "SELECT BuyerInfo.*, UsersInfo.firstName as agentF, UsersInfo.lastName as agentL, UsersInfo.email as agentEmail, UsersInfo.phone as agentPhone, 
+                    HouseInfo.address
+                    FROM BuyerInfo LEFT JOIN UsersInfo ON BuyerInfo.userId = UsersInfo.userId LEFT JOIN HouseInfo ON BuyerInfo.houseId = HouseInfo.houseId";
+
+$visitorStmt = $dbConn->prepare($sqlGetVisitors);
+$visitorStmt->execute();
+$visitorResults = $visitorStmt->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>RE/MAX Salinas | OverSite Admin</title>
+
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
+        <!-- BEGIN TEMPLATE default-css.php INCLUDE -->
+        <?php include "templates-osa/default-css.php" ?>
+        <!-- END TEMPLATE default-css.php INCLUDE -->
+
+        <!-- PAGE-SPECIFIC CSS -->
+        <link rel="stylesheet" href="../dist/css/vendor/footable.bootstrap.min.css">
+        <link rel="stylesheet" href="../plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+        <link rel="stylesheet" href="../dist/css/vendor/fullcalendar.min.css">
+
+        <!-- iCheck for checkboxes and radio inputs -->
+        <link rel="stylesheet" href="../plugins/iCheck/all.css">
+    </head>
+
+    <body class="hold-transition skin-black sidebar-mini">
+
+        <!-- Site Wrapper -->
+        <div class="wrapper">
+
+            <!-- BEGIN TEMPLATE header.php INCLUDE -->
+            <?php include "templates-osa/header.php" ?>
+            <!-- END TEMPLATE header.php INCLUDE -->
+
+            <!-- BEGIN TEMPLATE nav.php INCLUDE -->
+            <?php include "templates-osa/nav.php" ?>
+            <!-- END TEMPLATE nav.php INCLUDE -->
+
+
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Admin View
+                        <!-- <small>Week Overview</small> -->
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li>Overview</li>
+                        <li class="active"><a href="#"><i class="fa fa-dashboard"></i> Staff Dashboard</a></li>
+                    </ol>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <!-- Small boxes (Stat box) -->
+
+
+                    <div class="row">
+
+                        <div class="col-xs-12">
+                            <div class="box">
+
+                                <div class="box-body">
+                                    <div class="box-body no-padding" style="height:600px; overflow:auto;">
+                                        <!-- THE CALENDAR -->
+                                        <!-- <div id="calendar"></div> -->
+                                        <table class="table table-striped" data-filtering="true">
+                                            <thead>
+                                                <tr>
+                                                    <th>Type</th>
+                                                    <th>Agent</th>
+                                                    <th>ID</th>
+                                                    <th>Name</th>
+                                                    <th>Phone</th>
+                                                    <th>Email</th>
+                                                    <th>Property</th>
+                                                    <th>How soon?</th>
+                                                    <th>Price</th>
+                                                    <th>Bedroom</th>
+                                                    <th>Bathroom</th>
+                                                    <th>Delete</th>
+                                                    <th data-breakpoints="all">Agent Email</th>
+                                                    <th data-breakpoints="all">Agent Phone</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                               <?php
+                                                    foreach ($visitorResults as $lead) 
+                                                    {
+                                                        echo "<tr>";
+                                                        echo "<td>" . $lead['houseId'] . "</td>";
+                                                        echo "<td>" . $lead['agentF'] . " " . $lead['agentL'] . "</td>";
+                                                        echo "<td>" . $lead['buyerID'] . "</td>";
+                                                        echo "<td>" . $lead['firstName'] . " " . $lead['lastName'] . "</td>";
+                                                        echo "<td>" . $lead['phone'] . "</td>";
+                                                        echo "<td>" . $lead['email'] . "</td>";
+                                                        echo "<td>" . $lead['address']. "</td>";
+                                                        echo "<td>" . $lead['howSoon'] . "</td>";
+                                                        echo "<td>" . $lead['priceMax'] . "</td>";
+                                                        echo "<td>" . $lead['bedroomsMin'] . "</td>";
+                                                        echo "<td>" . $lead['bathroomsMin'] . "</td>";
+                                                        echo "<td><button>Delete</button></td>";
+                                                        echo "<td>" . $lead['agentEmail'] . "</td>";
+                                                        echo "<td>" . $lead['agentPhone'] . "</td>";
+                                                        echo "</tr>";
+                                                    }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <!-- /.box-body -->
+                            </div>
+                            <!-- /.box -->
+                        </div>
+                        <div class="col-xs-3">
+
+                            
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </section>
+            </div>
+            <!-- /.content-wrapper -->
+        </div>
+        <!-- /.wrapper -->
+        
+
+        <!-- BEGIN TEMPLATE default-footer.php INCLUDE -->
+        <?php include "templates-osa/default-footer.php" ?>
+        <!-- END TEMPLATE default-footer.php INCLUDE -->
+
+        <!-- BEGIN TEMPLATE default-js.php INCLUDE -->
+        <?php include "templates-osa/default-js.php" ?>
+        <!-- END TEMPLATE default-js.php INCLUDE -->
+
+        <!-- PAGE-SPECIFIC JS -->
+        <script src="../dist/js/vendor/footable.min.js"></script>
+        <!-- PAGE-SPECIFIC JS -->
+        <script src="../dist/js/vendor/moment-with-locales.min.js"></script>
+        <!--        <script src="../dist/js/vendor/fullcalendar/gcal.min.js"></script>-->
+        <script src="../dist/js/vendor/fullcalendar/fullcalendar.min.js"></script>
+
+
+
+
+
+        <script>
+            jQuery(function($) {
+                $('.table').footable({
+                     "sorting": {
+                "enabled": true
+                },
+                "filtering": {
+                "connectors": false
+                }
+                });
+            });
+
+        </script>
+        <script>
+            
+
+        </script>
+
+        <script>
+           
+
+        </script>
+        <script src="../plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+
+       
+
+    </body>
+
+</html>
---
+++ osa/templates-osa/nav.php
@@ -0,0 +1,27 @@
+<?php
+
+// $sql = "SELECT * FROM UsersInfo WHERE userType = :userType";
+
+// $namedParameters = array();
+// $namedParameters[':userType'] = "1";
+// $stmt = $dbConn->prepare($sql);
+// $stmt->execute($namedParameters);
+// $agentResults = $stmt->fetchAll();
+
+?>
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- Sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <a href="index.php" class="logo">
+            <!-- Logo -->
+            <img class="img-responsive" src="../dist/img/remax-logo.png">
+        </a>
+        <!-- Sidebar Menu -->
+          <ul class="sidebar-menu" data-widget="tree">
+             <li><a href="./index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+         </ul>
+        <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
