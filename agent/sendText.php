d900ee5b71564a376a348e7cb6f8ee9fe53fc2b6
Jose Diaz <jodiaz@csumb.edu>
2018-06-08 15:25:37 -0700

    adding disclaimer when texting from visitors page

--- agent/sendText.php
+++ agent/sendText.php
@@ -3,8 +3,19 @@ session_start();
 
 require '../keys/cred.php';
 require '../twilio-php-master/Twilio/autoload.php';
-
+require '../databaseConnection.php';
 use Twilio\Rest\Client;
+
+$dbConn = getConnection();
+
+$agentEmailSql = "SELECT firstName, lastName, email, phone FROM UsersInfo WHERE userId = :userId";
+$stmt = $dbConn->prepare($agentEmailSql);
+$namedParameters = array();
+$namedParameters[':userId'] = $_SESSION['userId'];
+$stmt->execute($namedParameters);
+$result = $stmt->fetch();
+
+
 
 $twilio_phone_number = "+18315851661";
 $client = new Client($sid, $token);
@@ -12,7 +23,8 @@ $client->messages->create(
     $_POST['phone'],
     array(
         "From" => $twilio_phone_number,
-        "Body" => $_POST['text'],
+        "Body" => $_POST['text'] . " %0a" . 
+        "Please don’t respond to this text. Please contact " . $result['firstName'] . " " . $result['lastName'] . "  at: " . $result['phone'] ,
     )
 );
 
