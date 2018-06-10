808424742b98e7f5b2f762771976668200d6d2bf
Jose Diaz <jodiaz@csumb.edu>
2018-06-10 14:43:48 -0700

    testing

--- osa/index.php
+++ osa/index.php
@@ -20,7 +20,7 @@ $dbConn = getConnection();
 $sqlGetVisitors = "SELECT BuyerInfo.*, UsersInfo.firstName as agentF, UsersInfo.lastName as agentL, UsersInfo.email as agentEmail, UsersInfo.phone as agentPhone, 
                     HouseInfo.address
                     FROM BuyerInfo LEFT JOIN UsersInfo ON BuyerInfo.userId = UsersInfo.userId LEFT JOIN HouseInfo ON BuyerInfo.houseId = HouseInfo.houseId WHERE 
-                    junk != 'yes'";
+                    junk != 'yes' ";
 
 $visitorStmt = $dbConn->prepare($sqlGetVisitors);
 $visitorStmt->execute();
