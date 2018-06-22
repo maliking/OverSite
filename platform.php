a43384b71f0ec330024af6d871a8350cd9006c2b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2018-06-21 19:51:18 -0500

    Facebook login

---
+++ platform.php
@@ -0,0 +1,27 @@
+<h2>MY platform</h2>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '177243886462061',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.0'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  function myFacebookLogin(){
+  	FB.login(function(){}, {scope: ''});
+  }
+
+</script>
+
+<button onstick="myFacebookLogin()">Login with Facebook</button>
