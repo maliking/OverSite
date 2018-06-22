cfda964a7eacb40d109810c6b90b95ff3e0cbc1f
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2018-06-21 19:59:29 -0500

    Facebook login

--- platform.php
+++ platform.php
@@ -18,11 +18,16 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 
-  function myFacebookLogin(){
-  	FB.login(function(response){
-  		console.log("Successfully logged in", response);
-  	}, {scope: 'manage_pages'});
-  }
+FB.login(function(response) {
+    if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+    } else {
+     console.log('User cancelled login or did not fully authorize.');
+    }
+});
 
 </script>
 
