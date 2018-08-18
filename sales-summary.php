81adae3734e9f8163ed4964e2763d96f5141748b
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2018-08-18 15:28:03 -0700

    Created individual sales breakdown page

--- sales-summary.php
+++ sales-summary.php
@@ -36,7 +36,7 @@ $avgPercentResult = $avgPercentStmt->fetch();
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>RE/MAX Salinas | Sales Breakdown</title>
+    <title>RE/MAX Salinas | Sales Breakdowns</title>
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- BEGIN TEMPLATE default-css.php INCLUDE -->
