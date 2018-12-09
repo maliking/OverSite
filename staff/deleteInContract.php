79e68356c6655e28ba2475a50e5d6c972018ca4b
Jose Diaz <jodiaz@csumb.edu>
2018-12-08 22:48:30 -0800

    adding notification when deleting in contract

--- staff/deleteInContract.php
+++ staff/deleteInContract.php
@@ -2,6 +2,13 @@
 
 session_start();
 require '../databaseConnection.php';
+
+require '../keys/cred.php';
+require '../twilio-php-master/Twilio/autoload.php';
+
+use Twilio\Rest\Client;
+
+
 $dbConn = getConnection();
 
 $sql = "DELETE FROM transactions 
@@ -9,5 +16,32 @@ $sql = "DELETE FROM transactions
 
 $stmt = $dbConn->prepare($sql);
 $stmt->execute();
+
+
+$transInfoSql = "SELECT address FROM transactions WHERE transId = :transId";
+$transParam = array();
+$transParam[':transId'] = $_POST['inContractId'];
+
+$transInfoStmt = $dbConn->prepare($transInfoSql);
+$transInfoStmt->execute($transParam);
+
+$transInfoResults = $transInfoStmt->fetch();
+
+
+$sendToNums = array("8319059490", "8313206212");
+$twilio_phone_number = "+18315851661";
+// if($houseId == "89")
+// {
+foreach ($sendToNums as $sendTo) 
+{
+	$client = new Client($sid, $token);
+	$client->messages->create(
+    $sendTo,
+    array(
+        "From" => $twilio_phone_number,
+        "Body" => $transInfoResults['address'] . " Deleted from In-Contract",
+    )
+);
+}
 
 ?>
