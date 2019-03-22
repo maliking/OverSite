97e9c766635a3f35dbacb7cd1eeaebd3bef8d6cf
Jose Diaz <jodiaz@csumb.edu>
2019-03-22 00:36:47 -0700

    fixing sending comm sheet error

--- sendCommissionSheet.php
+++ sendCommissionSheet.php
@@ -251,136 +251,137 @@ $documentFileName = "uploadFlyers/" . $houseListingId . ".pdf";
 	
 	// Set Authentication information
 	// Set via a config file or just set here using constants.
-	$email = $username;	// your account email.
-	$password = $password;		// your account password
-	$integratorKey = $intKey; // your account integrator key, found on (Preferences -> API page)
+	// $email = $username;	// your account email.
+	// $password = $password;		// your account password
+	// $integratorKey = $intKey; // your account integrator key, found on (Preferences -> API page)
 	// api service point
-	$url = "https://demo.docusign.net/restapi/v2/login_information"; // change for production
+	// $url = "https://demo.docusign.net/restapi/v2/login_information"; // change for production
 	///////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////
 	//
 	// Start...
 	// construct the authentication header:
-	$header = "<DocuSignCredentials><Username>" . $email . "</Username><Password>" . $password . "</Password><IntegratorKey>" . $integratorKey . "</IntegratorKey></DocuSignCredentials>";
+	// $header = "<DocuSignCredentials><Username>" . $email . "</Username><Password>" . $password . "</Password><IntegratorKey>" . $integratorKey . "</IntegratorKey></DocuSignCredentials>";
 	/////////////////////////////////////////////////////////////////////////////////////////////////
 	// STEP 1 - Login (to retrieve baseUrl and accountId)
 	/////////////////////////////////////////////////////////////////////////////////////////////////
-	$curl = curl_init($url);
-	curl_setopt($curl, CURLOPT_HEADER, false);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-DocuSign-Authentication:" . $header));
-	$json_response = curl_exec($curl);
-	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	if ( $status != 200 ) {
-		return (['ok' => false, 'errMsg' => "Error calling DocuSign, status is: " . $status]);
-	}
-	$response = json_decode($json_response, true);
-	$accountId = $response["loginAccounts"][0]["accountId"];
-	$baseUrl = $response["loginAccounts"][0]["baseUrl"];
-	curl_close($curl);
-	echo $response;
+	// $curl = curl_init($url);
+	// curl_setopt($curl, CURLOPT_HEADER, false);
+	// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-DocuSign-Authentication:" . $header));
+	// $json_response = curl_exec($curl);
+	// $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	// if ( $status != 200 ) {
+	// 	return (['ok' => false, 'errMsg' => "Error calling DocuSign, status is: " . $status]);
+	// }
+	// $response = json_decode($json_response, true);
+	// $accountId = $response["loginAccounts"][0]["accountId"];
+	// $baseUrl = $response["loginAccounts"][0]["baseUrl"];
+	// curl_close($curl);
+	// echo $response;
 	/////////////////////////////////////////////////////////////////////////////////////////////////
 	// STEP 2 - Create and send envelope with one recipient, one tab, and one document
 	/////////////////////////////////////////////////////////////////////////////////////////////////
 	// the following envelope request body will place 1 signature tab on the document, located
 	// 100 pixels to the right and 100 pixels down from the top left of the document
-	$data = 
-		array (
-			"emailSubject" => "DocuSign API - Please sign " . $documentName,
-			"documents" => array( 
-				array("documentId" => "1", "name" => $documentName)
-				),
-			"recipients" => array( 
-				"signers" => array(
-					array(
-						"email" => $userResults['email'],
-						"name" => $userResults['firstName'] . " " . $userResults['lastName'],
-						"recipientId" => "1",
-						"routingOrder" => "1",
-						"tabs" => array(
-							"signHereTabs" => array(
-								array(
-									"xPosition" => "60",
-									"yPosition" => "555",
-									"documentId" => "1",
-									"pageNumber" => "1"
-								)
-							),
-							"dateSignedTabs" => array(
-								array(
-									"xPosition" => "140",
-									"yPosition" => "595",
-									"documentId" => "1",
-									"pageNumber" => "1"
-									)
-								)
-						)
-					),
-					array(
-						"email" => $currAgentEmail['email'],
-						"name" => $currAgentEmail['firstName'] . " " . $currAgentEmail['lastName'],
-						"recipientId" => "2",
-						"routingOrder" => "2",
-						"tabs" => array(
-							"signHereTabs" => array(
-								array(
-									"xPosition" => "340",
-									"yPosition" => "647",
-									"documentId" => "1",
-									"pageNumber" => "1"
-								)
-							),
-							"dateSignedTabs" => array(
-								array(
-									"xPosition" => "500",
-									"yPosition" => "687",
-									"documentId" => "1",
-									"pageNumber" => "1"
-									)
-								)
-						)
-					)
-				)
-			),
-		"status" => "sent"
-	);
-	$data_string = json_encode($data);  
-	$file_contents = file_get_contents($documentFileName);
-	// Create a multi-part request. First the form data, then the file content
-	$requestBody = 
-		 "\r\n"
-		."\r\n"
-		."--myboundary\r\n"
-		."Content-Type: application/json\r\n"
-		."Content-Disposition: form-data\r\n"
-		."\r\n"
-		."$data_string\r\n"
-		."--myboundary\r\n"
-		."Content-Type:application/pdf\r\n"
-		."Content-Disposition: file; filename=\"$documentName\"; documentid=1 \r\n"
-		."\r\n"
-		."$file_contents\r\n"
-		."--myboundary--\r\n"
-		."\r\n";
-	// Send to the /envelopes end point, which is relative to the baseUrl received above. 
-	$curl = curl_init($baseUrl . "/envelopes" );
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $requestBody);                                                                  
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
-		'Content-Type: multipart/form-data;boundary=myboundary',
-		'Content-Length: ' . strlen($requestBody),
-		"X-DocuSign-Authentication: $header" )                                                                       
-	);
-	$json_response = curl_exec($curl); // Do it!
-	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	if ( $status != 201 ) {
-		echo "Error calling DocuSign, status is:" . $status . "\nerror text: ";
-		print_r($json_response); echo "\n";
-		exit(-1);
-	}
-	$response = json_decode($json_response, true);
-	$envelopeId = $response["envelopeId"];
+	
+	// $data = 
+	// 	array (
+	// 		"emailSubject" => "DocuSign API - Please sign " . $documentName,
+	// 		"documents" => array( 
+	// 			array("documentId" => "1", "name" => $documentName)
+	// 			),
+	// 		"recipients" => array( 
+	// 			"signers" => array(
+	// 				array(
+	// 					"email" => $userResults['email'],
+	// 					"name" => $userResults['firstName'] . " " . $userResults['lastName'],
+	// 					"recipientId" => "1",
+	// 					"routingOrder" => "1",
+	// 					"tabs" => array(
+	// 						"signHereTabs" => array(
+	// 							array(
+	// 								"xPosition" => "60",
+	// 								"yPosition" => "555",
+	// 								"documentId" => "1",
+	// 								"pageNumber" => "1"
+	// 							)
+	// 						),
+	// 						"dateSignedTabs" => array(
+	// 							array(
+	// 								"xPosition" => "140",
+	// 								"yPosition" => "595",
+	// 								"documentId" => "1",
+	// 								"pageNumber" => "1"
+	// 								)
+	// 							)
+	// 					)
+	// 				),
+	// 				array(
+	// 					"email" => $currAgentEmail['email'],
+	// 					"name" => $currAgentEmail['firstName'] . " " . $currAgentEmail['lastName'],
+	// 					"recipientId" => "2",
+	// 					"routingOrder" => "2",
+	// 					"tabs" => array(
+	// 						"signHereTabs" => array(
+	// 							array(
+	// 								"xPosition" => "340",
+	// 								"yPosition" => "647",
+	// 								"documentId" => "1",
+	// 								"pageNumber" => "1"
+	// 							)
+	// 						),
+	// 						"dateSignedTabs" => array(
+	// 							array(
+	// 								"xPosition" => "500",
+	// 								"yPosition" => "687",
+	// 								"documentId" => "1",
+	// 								"pageNumber" => "1"
+	// 								)
+	// 							)
+	// 					)
+	// 				)
+	// 			)
+	// 		),
+	// 	"status" => "sent"
+	// );
+	// $data_string = json_encode($data);  
+	// $file_contents = file_get_contents($documentFileName);
+	// // Create a multi-part request. First the form data, then the file content
+	// $requestBody = 
+	// 	 "\r\n"
+	// 	."\r\n"
+	// 	."--myboundary\r\n"
+	// 	."Content-Type: application/json\r\n"
+	// 	."Content-Disposition: form-data\r\n"
+	// 	."\r\n"
+	// 	."$data_string\r\n"
+	// 	."--myboundary\r\n"
+	// 	."Content-Type:application/pdf\r\n"
+	// 	."Content-Disposition: file; filename=\"$documentName\"; documentid=1 \r\n"
+	// 	."\r\n"
+	// 	."$file_contents\r\n"
+	// 	."--myboundary--\r\n"
+	// 	."\r\n";
+	// // Send to the /envelopes end point, which is relative to the baseUrl received above. 
+	// $curl = curl_init($baseUrl . "/envelopes" );
+	// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// curl_setopt($curl, CURLOPT_POST, true);
+	// curl_setopt($curl, CURLOPT_POSTFIELDS, $requestBody);                                                                  
+	// curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
+	// 	'Content-Type: multipart/form-data;boundary=myboundary',
+	// 	'Content-Length: ' . strlen($requestBody),
+	// 	"X-DocuSign-Authentication: $header" )                                                                       
+	// );
+	// $json_response = curl_exec($curl); // Do it!
+	// $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	// if ( $status != 201 ) {
+	// 	echo "Error calling DocuSign, status is:" . $status . "\nerror text: ";
+	// 	print_r($json_response); echo "\n";
+	// 	exit(-1);
+	// }
+	// $response = json_decode($json_response, true);
+	// $envelopeId = $response["envelopeId"];
 	
 /////////////////////////////////////
 // $curl = curl_init();
@@ -468,9 +469,9 @@ $documentFileName = "uploadFlyers/" . $houseListingId . ".pdf";
 
 // }
 
-$envId = json_decode($response, true);
-$namedParameters[":envelopeId"] = $envelopeId;
-// $namedParameters[":envelopeId"] = "";
+// $envId = json_decode($response, true);
+// $namedParameters[":envelopeId"] = $envelopeId;
+$namedParameters[":envelopeId"] = "";
 $stmt = $dbConn->prepare($sql);
 try {
 
